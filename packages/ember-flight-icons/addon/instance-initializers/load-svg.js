6ea12a044921fa90196253af7ab1e8eb4fc482e6
Martin Henry <martin.henry@hashicorp.com>
2023-05-10 16:10:20 -0700

    change impprt

--- packages/ember-flight-icons/addon/instance-initializers/load-svg.js
+++ packages/ember-flight-icons/addon/instance-initializers/load-svg.js
@@ -1,4 +1,4 @@
-import svgSprite from '../svg-sprite/svg-sprite-module';
+import svgSprite from './../svg-sprite/svg-sprite-module';
 
 export function initialize(/* application */) {
   window.document.body.insertAdjacentHTML('beforeend', svgSprite)
