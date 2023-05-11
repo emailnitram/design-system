71592f4ef1d16acddec3573b98b0ea3db31cd167
Martin Henry <martin.henry@hashicorp.com>
2023-05-10 17:01:30 -0700

    remove imports

--- packages/ember-flight-icons/addon/instance-initializers/load-svg.js
+++ packages/ember-flight-icons/addon/instance-initializers/load-svg.js
@@ -1,7 +1,7 @@
-import svgSprite from './../svg-sprite/svg-sprite-module';
+// import svgSprite from './../svg-sprite/svg-sprite-module';
 
 export function initialize(/* application */) {
-  window.document.body.insertAdjacentHTML('beforeend', svgSprite)
+  // window.document.body.insertAdjacentHTML('beforeend', svgSprite)
 }
 
 export default {
